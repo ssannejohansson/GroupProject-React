d755ffe7fb538e904530ae1fcc7b7051b02ba74f
ssannejohansson <sannej0hansson@hotmail.com>
2025-05-08 09:51:09 +0200

    Fixed comments
    Made indentations on comments to make code more clear

--- group-project/src/components/Registration.jsx
+++ group-project/src/components/Registration.jsx
@@ -1,26 +1,27 @@
 import React from "react"
-import {useNavigate} from "react-router" // Need to be installed with React Router through " npm i react-router " in terminal
+import {useNavigate} from "react-router"                                // Need to be installed with React Router through " npm i react-router " in terminal
 import "./LoginRegistration.css"
-import { FaUser, FaLock } from "react-icons/fa"; // Needs to be installed with React Icons through " npm install react-icons --save " in terminal
+import { FaUser, FaLock } from "react-icons/fa";                        // Needs to be installed with React Icons through " npm install react-icons --save " in terminal
 
 
 function Registration ({formHandle}) {
    
-    const navigate = useNavigate (); // Navigation 
-    const loggeduser = JSON.parse(localStorage.getItem('user')); // Checks for the user in localStorage
+    const navigate = useNavigate ();                                    // Navigation 
+
+    const loggeduser = JSON.parse(localStorage.getItem('user'));        // Checks for the user in localStorage
 
 
 // Registration-logic
     const handleRegistration = (e) => {
-        e.preventDefault(); // Prevents browser from realod/refresh
+        e.preventDefault();                                             // Prevents browser from realod/refresh
         if (e.target.username.value && e.target.password.value) {
-          if (!localStorage.getItem("user")) { // If "user" is NOT found in localStorage...
+          if (!localStorage.getItem("user")) {                          // If "user" is NOT found in localStorage...
             localStorage.setItem("user", JSON.stringify({username:e.target.username.value, password:e.target.password.value})) // ... it sets a new user in localStorage.
-            alert ("Registration succeded!") // Alert that registration is succeded just to be clear to the user
-            navigate ('game'); // When alert "OK" is clicked it navigates to game-page 
+            alert ("Registration succeded!")                            // Alert that registration is succeded just to be clear to the user
+            navigate ('game');                                          // When alert "OK" is clicked it navigates to game-page 
           }
-        } if ((e.target.username.value === loggeduser.username)) { // Checks if the input-username is equal to the stored username
-            alert("User already exists"); // If the username already exists in localStorage, alert will show
+        } if ((e.target.username.value === loggeduser.username)) {      // Checks if the input-username is equal to the stored username
+            alert("User already exists");                               // If the username already exists in localStorage, alert will show
         }
     } 
 
